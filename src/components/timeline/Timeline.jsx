10bdef9fef8d02376ce4910110721e41a272cf3a
s.nakamura <shotenba@gmail.com>
2023-07-27 00:59:49 +0900

    コンポーネントの作成

---
+++ src/components/timeline/Timeline.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Timeline.css";
+
+export default function Timeline() {
+  return (
+    <div>
+      <p>timeline</p>
+    </div>
+  );
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import Topbar from '../../components/Topbar'
+import React from "react";
+import "./Home.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Timeline from "../../components/timeline/Timeline";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Topbar from "../../components/topbar/Topbar";
 
 export default function Home() {
   return (
     <>
       <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        {/* <Timeline />
+        <Rightbar /> */}
+      </div>
     </>
-  )
+  );
 }
