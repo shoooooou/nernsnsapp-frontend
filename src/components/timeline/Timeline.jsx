934fc41f2a274c1b0984db968eed19f7676b89da
s.nakamura <shotenba@gmail.com>
2023-07-29 17:31:05 +0900

    一旦コミット

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Timeline.css";
 import Share from "../share/Share";
+import {Posts} from "../../dummyData"
 
 export default function Timeline() {
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        {/* <Post/> */}
+        {Posts.map((post)=>(
+          <Post post={post} key={post.key}/>
+        ))}
       </div>
     </div>
   );
